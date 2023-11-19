69ed6df7379c611598cef394e1d0b2c2ad2941e3
anishah78 <anishahhussain@hotmail.com>
2023-11-19 17:06:45 +0000

    commit messages added

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Hangman
 
 # Table of Contents
+1. Description
+2. Installation 
 
-# 
 
 A description of the project: what it does, the aim of the project, and what you learned
 Installation instructions
--- milestone_3.py
+++ milestone_3.py
@@ -1,19 +1,23 @@
 import random
 words_list = ['strawberry', 'kiwi', 'pear', 'oranges', 'grapes']
+#list of words
 word = random.choice(words_list)
+#randomly chooses the word from the list
 def check_guess(guess):
+#convert the guess to lower case
     guess = guess.lower()
+#check if guess is in the word
     if guess in word:
         print(f'Good guess! {guess} is in the word.')
     else:
         print(f'Sorry, {guess} is not in the word. Try again.')
 def ask_for_input():
-    guess = '' 
+    guess = '' #initialised guess variable
     while True:
-        guess = input('Enter a single letter: ')
-        if len(guess) == 1 and guess.isalpha():
+        guess = input('Enter a single letter: ') #user inputs guess
+        if len(guess) == 1 and guess.isalpha(): #checks if guess is a single alphabetical letter
             break
         else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-    check_guess(guess)
-ask_for_input()
+            print("Invalid letter. Please, enter a single alphabetical character.") #asking user for valid input if incorrect 
+    check_guess(guess) 
+ask_for_input() #calls function after breaking out of the loop 
